f1c3abfbaa17c7abcb9e43b96ab50fac5cd7b232
GeorgeV431 <45008156+GeorgeV431@users.noreply.github.com>
2021-06-14 17:10:32 -0400

    avance formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormularioComponent } from './formulario/formulario.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +23,13 @@ import { FormularioComponent } from './formulario/formulario.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-formulario',
@@ -6,6 +8,104 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.scss']
 })
 export class FormularioComponent implements OnInit {
+
+  hideRequiredControl = new FormControl(false);
+  floatLabelControl = new FormControl('auto');
+
+  name = new FormControl('', [Validators.required,]);
+  apellido = new FormControl('', [Validators.required,]);
+  rut = new FormControl('', [Validators.required,]);
+  direccion = new FormControl('', [Validators.required,]);
+  correo = new FormControl('', [Validators.required,Validators.email]);
+  contrasenya = new FormControl('', [Validators.required,]);
+  cContrasenya = new FormControl('', [Validators.required,]);
+  region = new FormControl('', [Validators.required,]);
+  comuna = new FormControl('', [Validators.required,]);
+
+
+  /*-- Nombre --*/
+
+  getErrorMessageNombre() {
+    if (this.name.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.name.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  /*-- Apellidos --*/
+  getErrorMessageApellidos() {
+    if (this.apellido.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.apellido.hasError('email') ? 'Not a valid email' : '';
+  }
+  /*-- RUT --*/
+  getErrorMessageRUT() {
+    if (this.rut.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.name.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  /*-- Direccion --*/
+  getErrorMessageDireccion() {
+    if (this.direccion.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.name.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  /*-- Region  --*/
+  getErrorMessageRegion() {
+    if (this.region.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.name.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  /*-- Romuna  --*/
+  getErrorMessageComuna() {
+    if (this.comuna.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.name.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  /*-- Correo electronico --*/
+  getErrorMessageCorreo() {
+    if (this.correo.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.name.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  /*-- Contraseña --*/
+  getErrorMessageContrasenya() {
+    if (this.contrasenya.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.name.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  /*-- Confirmar Contraseña --*/
+  getErrorMessageCContrasenya() {
+    if (this.cContrasenya.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.name.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  
+  
 
   constructor() { }
 
