97fbbe8a31bee803d1dcf2be5093b59d8fd711d9
Aaron <aaron.frenkel@iguanabee.com>
2021-06-23 18:43:15 -0400

    Update tienda.component.ts

--- src/app/tienda/tienda.component.ts
+++ src/app/tienda/tienda.component.ts
@@ -15,8 +15,8 @@ export class TiendaComponent implements OnInit {
 
   num: Array<Producto> = [
     {nombre: "Shiba1", stock: 10, valor: 3000, categoria: "Pepperonni"},
-    {nombre: "Shiba3", stock: 10, valor: 1000, categoria: "Mushroom"},
-    {nombre: "Shiba2", stock: 10, valor: 5000, categoria: "Extra Cheese"},
+    {nombre: "Shiba2", stock: 10, valor: 1000, categoria: "Mushroom"},
+    {nombre: "Shiba3", stock: 10, valor: 5000, categoria: "Extra Cheese"},
     {nombre: "Shiba4", stock: 10, valor: 9000, categoria: "Pepperonni"},
     {nombre: "Shiba5", stock: 10, valor: 8000, categoria: "Mushroom"},
     {nombre: "Shiba6", stock: 10, valor: 10000, categoria: "Extra Cheese"},
@@ -45,6 +45,7 @@ export class TiendaComponent implements OnInit {
 
   show = true;
   onSubmit(){
+    
     this.show = false;
 
   }
