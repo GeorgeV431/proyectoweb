966c415619f5d53460848e71bcf4e10915c2f49e
Aaron <aaron.frenkel@iguanabee.com>
2021-06-28 16:42:04 -0400

    Revert "Revert "Revert "admin, get user y pedido"""
    This reverts commit 71c3010f47fc0121992c3b0565cb462623c26a3d.

--- src/app/admin-view/admin-view.component.ts
+++ src/app/admin-view/admin-view.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, HostBinding } from '@angular/core';
 //import { Usuario, Producto, Boleta, Detalle } from '../clases/clases';
 import { Usuario, Producto, Boleta, Detalle } from '../interfaces';
 import { ServicioService } from '../servicio.service';
-import { ApiService } from '../api.service'
 
 @Component({
   selector: 'app-admin-view',
@@ -18,18 +17,29 @@ export class AdminViewComponent implements OnInit {
 
   panelOpenState = false;
 
-  constructor(public servicio: ServicioService, public api: ApiService) { }
+  constructor(public servicio: ServicioService) { }
 
   ngOnInit() {
-    let token = this.servicio.datos.token;
+  }
 
-    this.api.getBoletas(token).subscribe(res=>{
-      this.pedidos = res;
-    });
+  getUsuarios() {
+    this.servicio.getUsuarios()
+      .subscribe(
+        res => {
+          this.usuarios = res;
+        },
+        err => console.error(err)
+      );
+  }
 
-    this.api.getUsuarios(token).subscribe(res=>{
-      this.usuarios = res;
-    });
+  getBoletas() {
+    this.servicio.getBoletas()
+      .subscribe(
+        res => {
+          this.pedidos = res;
+        },
+        err => console.error(err)
+      );
   }
 
 }
--- src/app/servicio.service.ts
+++ src/app/servicio.service.ts
@@ -7,20 +7,10 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ServicioService {
-  localStorageService;
-  sessionStorageService;
-  constructor(private http: HttpClient){
-    this.localStorageService = localStorage;
-    this.sessionStorageService = sessionStorage;
-  }
 
-  rawDatos:string ="";
-  datos = {
-    token:"",
-    email:"",
-    admin:2
-  };
   API_URI = 'http://localhost/boutique';
+
+  constructor(private http: HttpClient) { }
 
   // inicio GET, DELETE Y POST de usuario
   getUsuarios() {
@@ -81,62 +71,4 @@ export class ServicioService {
     return this.http.post(`${this.API_URI}/detalle`, detalle);
   }
   // fin GET, DELETE Y POST de Detalle
-
-  //  PARTE NUEVA
-  ///////////////
-  ///////////////
-
-  cargarDatos(token:string,email:string,admin:number){
-    console.log("cargar datos");
-    let datos = {
-      token:`${token}`,
-      email:`${email}`,
-      admin:admin
-    }
-    console.log(datos);
-    this.localStorageService.setItem('sesion', JSON.stringify(datos));
-    this.datos = datos;
-  }
-
-  borrarDatos(){
-    console.log('borrar datos');
-    this.localStorageService.removeItem('sesion');
-    this.datos = {
-      token:'',
-      email:'',
-      admin:2
-    }
-    this.borrarDatosSession();
-  }
-
-  getDatos(){
-    if(localStorage.getItem("sesion")!=null){
-      this.rawDatos = localStorage.getItem('sesion')!;
-      this.datos = JSON.parse(this.rawDatos);
-    }else{
-      console.log("no hay datos");
-    }
-  }
-
-  //--------------SESION STORAGE-----------------------
-
-  cargarDatosSession(token:string, email:string, admin:number){
-    console.log("SESSION STORAGE");
-      let datos ={
-        token:`${token}`,
-        email:`${email}`,
-        admin:admin
-      }
-      this.sessionStorageService.setItem('sesion',JSON.stringify(datos));
-      this.datos=datos;
-  }
-
-  borrarDatosSession(){
-    this.sessionStorageService.removeItem('sesion');
-    this.datos={
-      token:'',
-      email:'',
-      admin:2
-    }
-  }
 }
