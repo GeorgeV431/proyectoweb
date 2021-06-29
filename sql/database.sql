4066d56152414a1452b6fc04ddd9f9761834210b
Roberto Concha <roberto.concha.v@mail.pucv.cl>
2021-06-28 23:57:13 -0400

    give up en el login

--- sql/database.sql
+++ sql/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-06-2021 a las 04:02:50
+-- Tiempo de generación: 29-06-2021 a las 05:52:54
 -- Versión del servidor: 10.4.19-MariaDB
 -- Versión de PHP: 8.0.7
 
@@ -96,30 +96,31 @@ CREATE TABLE `producto` (
   `nombre` varchar(100) NOT NULL,
   `stock` int(3) NOT NULL,
   `valor` int(6) NOT NULL,
-  `categoria` varchar(50) NOT NULL
+  `categoria` varchar(50) NOT NULL,
+  `imagenes` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
-INSERT INTO `producto` (`id`, `nombre`, `stock`, `valor`, `categoria`) VALUES
-(1, 'Collar c/ flores', 50, 2000, 'Accesorios'),
-(2, 'Arnes', 50, 3980, 'Accesorios'),
-(3, 'Juguetes ', 0, 1000, 'Accesorios'),
-(4, 'Poleron Minie Mouse', 10, 6500, 'Ropa'),
-(5, 'Vestido Rosado', 10, 4890, 'Ropa'),
-(6, 'Vestido Azul', 10, 4890, 'Ropa'),
-(7, 'Enterito rosado/azul', 20, 8000, 'Ropa'),
-(8, 'Poleron de polar', 10, 3500, 'Ropa'),
-(9, 'Plato doble', 30, 2500, 'Platos'),
-(10, 'Plato redondo', 30, 1250, 'Platos'),
-(11, 'Plato Cuadrado', 20, 1300, 'Platos'),
-(12, 'Plato pequeño', 20, 2200, 'Platos'),
-(13, 'Cojin cuadrado', 10, 6000, 'Camas'),
-(14, 'Cama ovalada', 20, 5000, 'Camas'),
-(15, 'Cama c/ liston', 10, 7990, 'Camas'),
-(16, 'Cama reversible', 20, 17000, 'Camas');
+INSERT INTO `producto` (`id`, `nombre`, `stock`, `valor`, `categoria`, `imagenes`) VALUES
+(1, 'Collar c/ flores', 50, 2000, 'Accesorios', 'https://i.imgur.com/tNzg5Qo.jpg'),
+(2, 'Arnes', 50, 3980, 'Accesorios', 'https://i.imgur.com/qqDO9JV.jpg'),
+(3, 'Juguetes ', 0, 1000, 'Accesorios', 'https://i.imgur.com/F13droe.jpg'),
+(4, 'Poleron Minie Mouse', 10, 6500, 'Ropa', 'https://i.imgur.com/oUHqJhA.jpg'),
+(5, 'Vestido Rosado', 10, 4890, 'Ropa', 'https://i.imgur.com/mxqJtzy.jpg'),
+(6, 'Vestido Azul', 10, 4890, 'Ropa', 'https://i.imgur.com/PBZ5TvQ.jpg'),
+(7, 'Enterito rosado/azul', 20, 8000, 'Ropa', 'https://i.imgur.com/IddJndK.jpg'),
+(8, 'Poleron de polar', 10, 3500, 'Ropa', 'https://i.imgur.com/HVvXzyr.jpg'),
+(9, 'Plato doble', 30, 2500, 'Platos', 'https://i.imgur.com/Q8NeZ46.png'),
+(10, 'Plato redondo', 30, 1250, 'Platos', 'https://i.imgur.com/pjKnWPi.jpg'),
+(11, 'Plato Cuadrado', 20, 1300, 'Platos', 'https://i.imgur.com/HtxKt4G.jpg'),
+(12, 'Plato pequeño', 20, 2200, 'Platos', 'https://i.imgur.com/09G4ovd.jpg'),
+(13, 'Cojin cuadrado', 10, 6000, 'Camas', 'https://i.imgur.com/4CjraIb.png'),
+(14, 'Cama ovalada', 20, 5000, 'Camas', 'https://i.imgur.com/R2KO4i1.jpg'),
+(15, 'Cama c/ liston', 10, 7990, 'Camas', 'https://i.imgur.com/7gcL3L0.jpg'),
+(16, 'Cama reversible', 20, 17000, 'Camas', 'https://i.imgur.com/KwmB5rF.jpg');
 
 -- --------------------------------------------------------
 
@@ -144,10 +145,10 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`Nombres`, `Apellidos`, `rut`, `Direccion`, `Comuna`, `Region`, `Correo`, `Password`, `Tipo`) VALUES
-('Aaron', 'Frenkel', 112223334, 'someplace', 'Rancagua', 'O\'Higgins', 'aaron@algo.cl', 'zxczxczxc', 1),
-('Jorge', 'Verschae', 223334445, 'somewhere', 'Rancagua', 'O\'Higgins', 'jorge@algo.cl', 'qweqweqwe', 1),
-('Roberto', 'Concha Vergara', 199037323, 'Portillo', 'Los Andes', 'Valparaiso', 'roberto@algo.cl', 'asdasdasd', 1),
-('User', 'Test', 123456789, 'boutique', 'Valparaiso', 'Valparaiso', 'usuario@boutique.cl', '1234', 0);
+('Aaron', 'Frenkel', 112223334, 'someplace', 'Rancagua', 'O\'Higgins', 'aaron@algo.cl', '7aa3262b9526ff30025c2f389263399e', 1),
+('Jorge', 'Verschae', 223334445, 'somewhere', 'Rancagua', 'O\'Higgins', 'jorge@algo.cl', 'b26986ceee60f744534aaab928cc12df', 1),
+('Roberto', 'Concha Vergara', 199037323, 'Portillo', 'Los Andes', 'Valparaiso', 'roberto@algo.cl', 'a3dcb4d229de6fde0db5686dee47145d', 1),
+('User', 'Test', 123456789, 'boutique', 'Valparaiso', 'Valparaiso', 'usuario@boutique.cl', '81dc9bdb52d04dc20036dbd8313ed055', 0);
 
 --
 -- Índices para tablas volcadas
